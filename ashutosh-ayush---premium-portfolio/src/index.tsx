4fb340b4c53ed408b3f13bbb5647047cc2019a13
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-11 01:23:21 +0530

    Package installed

--- ashutosh-ayush---premium-portfolio/src/index.tsx
+++ ashutosh-ayush---premium-portfolio/src/index.tsx
@@ -1,16 +1,8 @@
-// Fixed error: Removed invalid type reference directives for react and react-dom
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
+const container = document.getElementById("root");
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (!container) throw new Error("Root container missing");
+
+createRoot(container).render(<App />);
